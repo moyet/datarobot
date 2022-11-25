9d5fcc6ba562c6a4c9dc113d6b1c700154021f4f
Magnus Dreyer <magnus.dreyer@gmail.com>
2022-11-25 12:28:23 +0100

    moved create-app to app.py

--- datarobot/app.py
+++ datarobot/app.py
@@ -1,23 +1,44 @@
 import logging
 import os
 
-from flask import request, Response
+from flask import Flask, request, Response
 from werkzeug import utils
 from sklearn import svm
 from joblib import dump, load
 from pandas import read_csv
-
-from datarobot import create_app
 
 UPLOAD_DIR = '/tmp/models/'
 
 if not os.path.exists(UPLOAD_DIR):
     os.mkdir(UPLOAD_DIR)
 
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='clojure-rules',
+        )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    return app
+
 app = create_app()
 
 
 def create_model(dataframe, target):
+    """ Created a model from a dataframe, with the specified target"""
     y = dataframe.pop(target).values
     X = dataframe.values
 
@@ -33,13 +54,17 @@ def create_model(dataframe, target):
 
 
 def save_model(model):
-    # This is unsafe, but since I have control over who can post to this API
+    """
+        Saves a model to the file system.
+        This is unsafe, but since I have control over who can post to this API
+
+    """
     filename = 'iris.model'
     dump(model, utils.safe_join(UPLOAD_DIR, filename))
 
 
 def load_model():
-    # I know that this could be unsafe
+    """Loads a previoous generated model from the upload dir"""
     filename = 'iris.model'
     model = load(utils.safe_join(UPLOAD_DIR, filename))
     return model
@@ -76,6 +101,7 @@ def create():
 
 @app.route('/hello', methods=['GET'])
 def hello():
+    """Small test to see if, the API is up and running"""
     return 'Hello DataRobot!'
 
 
